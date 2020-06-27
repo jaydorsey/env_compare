f2a0e098cb45f8d2cf64e50a20013b128ae965b4
Jay Dorsey <jaydorsey@fastmail.com>
2020-06-26 17:34:35 -0700

    Move dependencies

--- lib/env_compare.rb
+++ lib/env_compare.rb
@@ -4,10 +4,6 @@ require 'env_compare/errors'
 require 'env_compare/name_space'
 require 'env_compare/version'
 require 'env_compare/cli'
-require 'erb'
-require 'launchy'
-require 'securerandom'
-require 'pry'
 
 module EnvCompare
 end
